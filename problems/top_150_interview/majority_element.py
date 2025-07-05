3c6fef00e3c98a7c771afd30b2e709d65b2828da
guilherme lopes <guilherme.lopes@dcx.ufpb.br>
2025-07-05 10:09:08 -0300

    new answer

--- problems/top_150_interview/majority_element.py
+++ problems/top_150_interview/majority_element.py
@@ -1,14 +1,12 @@
-nums = [6, 6, 6, 7, 7]
-stack = nums
-most_common = 0
+from collections import Counter
 
-if len(stack) == 1:
-    most_common = stack[0]
-else:
-    for num in nums:
-        if num in stack:
-            stack.remove(num)
-            if num in nums and num in stack:
-                most_common = num
-print(stack)
-print(most_common)
+def majorityElement(nums: List[int]) -> int:
+    count = Counter(nums)
+    n = len(nums) // 2
+    print(count)
+    for num, cnt in count.items():
+        if cnt > n:
+            return num
+
+# using the Counter method can be more efficient because it organizes the counts in a single pass, 
+# in a dictionary-like structure. 
