1eea42caf46eb68f6887d7dc9a20d6954569dc20
guilherme lopes <guilherme.lopes@dcx.ufpb.br>
2025-07-16 14:03:06 -0300

    new answer

--- problems/top_150_interview/happy_num.py
+++ problems/top_150_interview/happy_num.py
@@ -1,3 +1,9 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        
+        str_num = str(n)
+        seen = set()
+        while str_num not in seen:
+            seen.add(str_num)
+            total = sum(int(digit) ** 2 for digit in str_num)
+            str_num = str(total)
+        return False if str_num != '1' else True
