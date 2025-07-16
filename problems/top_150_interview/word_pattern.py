18e18041ae62298968aa3c0747d7e9f32777b50c
guilherme lopes <guilherme.lopes@dcx.ufpb.br>
2025-07-16 13:55:13 -0300

    new answer

--- problems/top_150_interview/word_pattern.py
+++ problems/top_150_interview/word_pattern.py
@@ -1,14 +1,15 @@
-def word_pattern(pattern: str, s: str) -> bool:
-    split = s.split()
-    if len(pattern) != len(split): # if the lengths are different, they're not a match
-        return False
-    mapping = {}
-    for p, word in zip(pattern, split):
-        if p in mapping:
-            if mapping[p] != word:
-                return False
-        else:
-            if word in mapping.values():
-                return False
-            mapping[p] = word
-    return True
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        split = s.split()
+        if len(pattern) != len(split):
+            return False
+        mapping = {}
+        for p, word in zip(pattern, split):
+            if p in mapping:
+                if mapping[p] != word:
+                    return False
+            else:
+                if word in mapping.values():
+                    return False
+                mapping[p] = word
+        return True
